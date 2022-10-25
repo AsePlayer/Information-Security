d7b5fe42109935bfe11ce914226d0dd47a6a0125
AsePlayer <52973701+AsePlayer@users.noreply.github.com>
2022-10-24 18:54:46 -0700

    Accordion Formatting with jQuery

--- Secure Web Application Programming/root/search_keyword.php
+++ Secure Web Application Programming/root/search_keyword.php
@@ -1,20 +1,43 @@
+<head>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+  
+  <style>
+	* {
+		font-family:Arial, Helvetica, sans-serrif;
+	}
+  </style>
+</head>
+
+
 <?php
 include "db_connect.php";
 
 $keyword_from_form = $_GET["keyword"];
 
-// Search for mama
-echo "<h2>Show all words with the joke '$keyword_from_form'</h2>";
+echo "<h1>Looking for jokes that contain the word '<em>$keyword_from_form</em>'</h1>";
 $sql = "SELECT JokeID, Joke_question, Joke_answer FROM jokes_table WHERE Joke_question LIKE '%" .  $keyword_from_form  ."%'";
 $result = $mysqli->query($sql);
 
+?>
+
+<div id="accordion">
+
+<?php
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["JokeID"]. " - Joke: " . $row["Joke_question"]. " Punchline: " . $row["Joke_answer"]. "<br>";
+	echo "<h3>$row[Joke_question]</h3>";
+	echo "<div> <p>$row[Joke_answer]</p> </div>";
   }
-} else {
-  echo "0 results";
 }
-
 ?>
+</div>
+<a href="index.php">Return to main page</a>
