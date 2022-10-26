9767d3b40e6ee8b5543392aa8afedc81ac5f176a
AsePlayer <52973701+AsePlayer@users.noreply.github.com>
2022-10-25 20:20:27 -0700

    Fixed bugs

--- Secure Web Application Programming/root/search_all_jokes.php
+++ Secure Web Application Programming/root/search_all_jokes.php
@@ -1,14 +1,14 @@
 <?php
 
-$sql = "SELECT JokeID, Joke_question, Joke_answer FROM jokes_table";
+$sql = "SELECT JokeID, Joke_question, Joke_answer, users_id FROM jokes_table";
 $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-	  echo "<h3>" . $row('Joke_question'] . "</h3>";
+	  echo "<h3>" . $row['Joke_question'] . "</h3>";
 	  
-	  echo "<div><p>" . $row['Joke_answer'] . " submitted by " . $row['users_id'] . "</p></div>" .  
+	  echo "<div><p>" . $row['Joke_answer'] . " submitted by " . $row['users_id'] . "</p></div>"; 
   }
 } else {
   echo "0 results";
